b81082d61a5562d519e6b29b0406104f553695ac
Jasson Caceres <jassoncaceres@Jassons-MBP.lan>
2023-12-24 00:03:57 -0500

    commit 2.1

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
           <ImageComponent
             src={
               "https://images.unsplash.com/photo-1543510875-8b2e1c93c1ed?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
+            } 
             alt={"image 1"}
           />
         </div>
